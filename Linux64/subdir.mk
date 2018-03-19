32f0f919c08c0788407c7674051290a0a99c2380
DjThunder <contact@b3dgs.com>
2018-03-19 19:33:35 +0100

    Makefile fixed.

--- Linux64/subdir.mk
+++ Linux64/subdir.mk
@@ -17,7 +17,7 @@ CPP_DEPS += \
 %.o: ../%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -std=c++0x -D_LINUX -D_GLIBCXX_USE_CXX11_ABI=0 -DPA_USE_ALSA=1 -I"$(abspath $(CURDIR)/../portaudio/include" -I"$(abspath $(CURDIR)/../portaudio/src/os/unix" -I"$(abspath $(CURDIR)/../libbinio/contrib/vs2015/libbinio" -I"$(abspath $(CURDIR)/../portaudio/src/common" -I"$(abspath $(CURDIR)/../libbinio/src" -I"$(abspath $(CURDIR)/../libbinio" -I"$(abspath $(CURDIR)/../src" -O3 -c -fmessage-length=0 -Wno-write-strings -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	g++ -std=c++0x -D_LINUX -D_GLIBCXX_USE_CXX11_ABI=0 -DPA_USE_ALSA=1 -I"$(abspath $(CURDIR)/../portaudio/include") -I"$(abspath $(CURDIR)/../portaudio/src/os/unix") -I"$(abspath $(CURDIR)/../libbinio/contrib/vs2015/libbinio") -I"$(abspath $(CURDIR)/../portaudio/src/common") -I"$(abspath $(CURDIR)/../libbinio/src") -I"$(abspath $(CURDIR)/../libbinio") -I"$(abspath $(CURDIR)/../src") -O3 -c -fmessage-length=0 -Wno-write-strings -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
